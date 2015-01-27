54818323db628da0656841dd03dd1bccfd425803
erwan.henry <erwanhenry@hotmail.com>
2015-01-27 15:51:30 +0100

    App fonctionne

--- app/src/main/java/com/jades/codeal/MainActivity.java
+++ app/src/main/java/com/jades/codeal/MainActivity.java
@@ -30,63 +30,10 @@ public class MainActivity extends Activity{
         setContentView(R.layout.activity_main);
 
         Log.d("test","test");
-    //    maListViewPerso = (ListView) findViewById(R.id.listviewperso);
-
-        //Création de la ArrayList qui nous permettra de remplire la listView
-        ArrayList<HashMap<String, String>> listItem = new ArrayList<HashMap<String, String>>();
-
-        //On déclare la HashMap qui contiendra les informations pour un item
-        HashMap<String, String> map;
-
-        //Création d'une HashMap pour insérer les informations du premier item de notre listView
-        map = new HashMap<String, String>();
-        //on insère un élément titre que l'on récupérera dans le textView titre créé dans le fichier affichageitem.xml
-        map.put("titre", "Erwan");
-        //on insère un élément description que l'on récupérera dans le textView description créé dans le fichier affichageitem.xml
-        map.put("Canette 33cL", "2€");
-        //on insère la référence à l'image (convertit en String car normalement c'est un int) que l'on récupérera dans l'imageView créé dans le fichier affichageitem.xml
-        map.put("img",String.valueOf(R.drawable.erwan));
-        //enfin on ajoute cette hashMap dans la arrayList
-        listItem.add(map);
-
-        map = new HashMap<String, String>();
-        map.put("titre", "Anto");
-        map.put("Canette 33cL", "2.1€");
-        map.put("img", String.valueOf(R.drawable.anto));
-        listItem.add(map);
-
-
-
-        //Création d'un SimpleAdapter qui se chargera de mettre les items présent dans notre list (listItem) dans la vue affichageitem
-        SimpleAdapter mSchedule = new SimpleAdapter (this.getBaseContext(), listItem, R.layout.activity_main,
-                new String[] {"img", "titre", "description"}, new int[] {R.id.img, R.id.titre, R.id.description});
-
-        //On attribut à notre listView l'adapter que l'on vient de créer
-        maListViewPerso.setAdapter(mSchedule);
-
-        //Enfin on met un écouteur d'évènement sur notre listView
-        maListViewPerso.setOnItemClickListener(new OnItemClickListener() {
-            @Override
-            @SuppressWarnings("unchecked")
-            public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-                //on récupère la HashMap contenant les infos de notre item (titre, description, img)
-                HashMap<String, String> map = (HashMap<String, String>) maListViewPerso.getItemAtPosition(position);
-                //on créer une boite de dialogue
-                AlertDialog.Builder adb = new AlertDialog.Builder(MainActivity.this);
-                //on attribut un titre à notre boite de dialogue
-                adb.setTitle("Sélection Item");
-                //on insère un message à notre boite de dialogue, et ici on affiche le titre de l'item cliqué
-                adb.setMessage("Votre choix : "+map.get("titre"));
-                //on indique que l'on veut le bouton ok à notre boite de dialogue
-                adb.setPositiveButton("Ok", null);
-                //on affiche la boite de dialogue
-                adb.show();
-            }
-        });
 
 
         //Création d'une instance de ma classe LivresBDD
-
+/*
         UserBDD userBdd = new UserBDD(this);
 
 
@@ -107,39 +54,11 @@ public class MainActivity extends Activity{
         //Pour vérifier que l'on a bien créé notre livre dans la BDD
         //on extrait le livre de la BDD grâce au titre du livre que l'on a créé précédemment
         User userFromBdd = userBdd.getUserWithNom(anto.getNom());
-        //Si un livre est retourné (donc si le livre à bien été ajouté à la BDD)
-        if(userFromBdd != null){
-            //On affiche les infos du livre dans un Toast
-            Toast.makeText(this, userFromBdd.toString(), Toast.LENGTH_LONG).show();
-            //On modifie le titre du livre
-            userFromBdd.setNom("sprite light");
-            //Puis on met à jour la BDD
-            userBdd.updateUser(userFromBdd.getId(), userFromBdd);
-        }
-        //On extrait le livre de la BDD grâce au nouveau titre
-        userFromBdd = userBdd.getUserWithNom("sprite light");
-        //S'il existe un livre possédant ce titre dans la BDD
-        if(userFromBdd != null){
-            //On affiche les nouvelle info du livre pour vérifié que le titre du livre a bien été mis à jour
-            Toast.makeText(this, userFromBdd.toString(), Toast.LENGTH_LONG).show();
-            //on supprime le livre de la BDD grâce à son ID
-            userBdd.removeUserWithId(userFromBdd.getId());
 
-        }
-        //On essait d'extraire de nouveau le livre de la BDD toujours grâce à son nouveau titre
-        userFromBdd = userBdd.getUserWithNom("sprite light");
-        //Si aucun livre n'est retourné
-        if(userFromBdd == null){
-            //On affiche un message indiquant que le livre n'existe pas dans la BDD
-            Toast.makeText(this, "Cet user n'existe pas dans la BDD", Toast.LENGTH_LONG).show();
-        }
-        //Si le livre existe (mais normalement il ne devrait pas)
-        else{
-            //on affiche un message indiquant que le livre existe dans la BDD
-            Toast.makeText(this, "Cet user existe dans la BDD", Toast.LENGTH_LONG).show();
-        }
 
         userBdd.close();
+
+        */
     }
 
 
